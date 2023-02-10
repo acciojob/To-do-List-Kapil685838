ed36ea109f728bde77d9f242d9858304f83fc4da
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-02-10 23:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-//your code here
 function addItem() {
 	var data = document.getElementById("newTodoInput").value;
-
 	if(data.length != 0){
 		var li = document.createElement("li");
 		li.innerHTML = data;
 		document.getElementById("todoList").appendChild(li);
 	}
-
 	document.getElementById("newTodoInput").value = "";
 }
